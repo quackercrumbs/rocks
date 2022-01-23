2d92b987ba199beaa22f4afc979a8a9f74890c2f
calvinqh <calvinquach@gmail.com>
2022-01-23 12:52:27 -0500

    Update importer with tokio, env logger and config parser
    importer just does a simple call to google but does not process the
    response

--- src/bin/importer.rs
+++ src/bin/importer.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Client;
+use configparser::ini::Ini;
+use env_logger;
+use env_logger::Env;
+
+#[macro_use]
+extern crate log;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting up");
+
+    // read configs
+    let mut private_config = Ini::new();
+    private_config.load("config/private.ini")?;
+    let nasa_private_key = private_config.get("topsecrets","NASA_API_KEY").expect("could not find NASA_API_KEY");
+
+    let client =  Client::new();
+    let uri = "http://google.com".parse()?;
+
+    let mut resp = client.get(uri).await?;
+
+    Ok(())
 }
