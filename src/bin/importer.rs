9e507a25365f76498bb5ad1ebc39042e999e7499
calvinqh <calvinquach@gmail.com>
2022-01-25 10:49:17 -0500

    Added serialization models to serialize API response
    All fields should be captured by the models now

--- src/bin/importer.rs
+++ src/bin/importer.rs
@@ -4,6 +4,7 @@ use hyper_tls::HttpsConnector;
 use configparser::ini::Ini;
 use env_logger;
 use env_logger::Env;
+use std::collections::HashMap;
 
 #[macro_use]
 extern crate log;
@@ -12,13 +13,55 @@ use serde_json;
 use serde::{Serialize,Deserialize};
 #[derive(Serialize,Deserialize,Debug)]
 struct Links {
-  next: String,
-  prev: String,
+  next: Option<String>,
+  prev: Option<String>,
+  #[serde(rename = "self")]
+  self_link: Option<String>
 }
 #[derive(Serialize,Deserialize,Debug)]
-struct AsteroidData {
+struct EstimatedDiameter {
+    estimated_diameter_min: f64,
+    estimated_diameter_max: f64
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct RelativeVelocity {
+    kilometers_per_second: String,
+    kilometers_per_hour: String,
+    miles_per_hour: String,
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct MissDistance {
+    astronomical: String,
+    lunar: String,
+    kilometers: String,
+    miles: String,
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct CloseApproachEvent {
+    close_approach_date: String,
+    close_approach_date_full: String,
+    epoch_date_close_approach: usize,
+    relative_velocity: RelativeVelocity,
+    miss_distance: MissDistance,
+    orbiting_body: String,
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct NearEarthObject {
+    links: Links,
+    id: String,
+    neo_reference_id: String,
+    nasa_jpl_url: String,
+    absolute_magnitude_h: f64,
+    // change string to some enum?
+    estimated_diameter: HashMap<String, EstimatedDiameter>,
+    close_approach_data: Vec<CloseApproachEvent>,
+    is_sentry_object: bool
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct NearEarthObjectResponse {
     links: Links,
     element_count: usize,
+    near_earth_objects: HashMap<String, Vec<NearEarthObject>>
 }
 
 #[tokio::main]
@@ -37,12 +80,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     info!("Making API call to {:?}", asteroid_uri);
     let mut resp = client.get(asteroid_uri).await?;
-    info!("response: {:?}", resp);
+    debug!("response: {:?}", resp);
     let body = resp.body_mut();
     let body_bytes = body::to_bytes(body).await?;
-    info!("response body: {:?}", body_bytes);
+    trace!("response body: {:?}", body_bytes);
 
-    let asteroid_data : AsteroidData = serde_json::from_slice(&body_bytes).unwrap();
+    let asteroid_data : NearEarthObjectResponse = serde_json::from_slice(&body_bytes).unwrap();
     info!("asteroid_data: {:?}", asteroid_data);
 
     Ok(())
