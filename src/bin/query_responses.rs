ee9e26fcd09abe7b746bfe0e3b1cd7f2bfae92e3
calvinqh <calvinquach@gmail.com>
2022-01-26 21:56:16 -0500

    refactor: move nasa models into separate file module

--- src/bin/query_responses.rs
+++ src/bin/query_responses.rs
@@ -1,6 +1,7 @@
 extern crate rocks;
 use rocks::schema::api_response::dsl;
 use rocks::db_util;
+use rocks::nasa;
 
 use diesel::prelude::*;
 use diesel::SqliteConnection;
@@ -13,63 +14,6 @@ use env_logger::Env;
 extern crate log;
 
 use serde_json;
-
-mod nasa_models {
-    use std::collections::HashMap;
-    use serde::{Serialize,Deserialize};
-    #[derive(Serialize,Deserialize,Debug)]
-    struct Links {
-      next: Option<String>,
-      prev: Option<String>,
-      #[serde(rename = "self")]
-      self_link: Option<String>
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    struct EstimatedDiameter {
-        estimated_diameter_min: f64,
-        estimated_diameter_max: f64
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    struct RelativeVelocity {
-        kilometers_per_second: String,
-        kilometers_per_hour: String,
-        miles_per_hour: String,
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    struct MissDistance {
-        astronomical: String,
-        lunar: String,
-        kilometers: String,
-        miles: String,
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    struct CloseApproachEvent {
-        close_approach_date: String,
-        close_approach_date_full: String,
-        epoch_date_close_approach: usize,
-        relative_velocity: RelativeVelocity,
-        miss_distance: MissDistance,
-        orbiting_body: String,
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    struct NearEarthObject {
-        links: Links,
-        id: String,
-        neo_reference_id: String,
-        nasa_jpl_url: String,
-        absolute_magnitude_h: f64,
-        // change string to some enum?
-        estimated_diameter: HashMap<String, EstimatedDiameter>,
-        close_approach_data: Vec<CloseApproachEvent>,
-        is_sentry_object: bool
-    }
-    #[derive(Serialize,Deserialize,Debug)]
-    pub struct NearEarthObjectResponse {
-        links: Links,
-        element_count: usize,
-        near_earth_objects: HashMap<String, Vec<NearEarthObject>>
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -98,7 +42,7 @@ async fn read_asteroid_api_responses(connection: &SqliteConnection) {
         debug!("response={:?}", record);
 
         // map raw responses onto 
-        let asteroid_response = serde_json::from_str::<nasa_models::NearEarthObjectResponse>(&record.response).expect("Could not serialize response");
+        let asteroid_response = serde_json::from_str::<nasa::models::NearEarthObjectResponse>(&record.response).expect("Could not serialize response");
         info!("serialized={:?}", asteroid_response);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate diesel;
 pub mod schema;
 pub mod models;
 pub mod db_util;
+pub mod nasa;
 
 pub fn helloworld() {
     println!("helloworld")
