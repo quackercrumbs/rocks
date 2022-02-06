f54090caa5e652134ec45ecc19c456d5d67f8808
Calvin <calvinquach@gmail.com>
2022-02-06 12:26:33 -0500

    update: setup initial world scene for rocks app

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,37 +1,42 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+fn initialize_world(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    // globe/earth
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 1.0, subdivisions: 2 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
 
-#[derive(Component)]
-struct Name(String);
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: false,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
 
-fn add_homies(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Calvin Quach".to_string()));
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
 }
 
-struct GreetTimer(Timer);
-fn greet_homies(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Welcome to the crib, {}", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
+pub struct RocksPlugin;
+impl Plugin for RocksPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_homies)
-        .add_system(greet_homies)
-        .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)));
+        app.add_startup_system(initialize_world);
     }
 }
 
 fn main() {
     App::new()
+        .insert_resource(Msaa {samples: 4})
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-
+        .add_plugin(RocksPlugin)
         .run();
 }
