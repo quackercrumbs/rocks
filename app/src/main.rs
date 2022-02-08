fa451cdb86bb377f8f2fd997cc8e955374079ac0
Calvin <calvinquach@gmail.com>
2022-02-07 20:43:24 -0500

    cleanup: camera orientation

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,26 +2,24 @@ use bevy::prelude::*;
 
 fn initialize_world(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     ass: Res<AssetServer>,
 ) {
     // note that we have to include the `Scene0` label
     let earth_model = ass.load("models/earth.glb#Scene0");
-
+    let earth_parent = (
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        GlobalTransform::identity(),
+    );
     // to be able to position our 3d model:
     // spawn a parent entity with a Transform and GlobalTransform
     // and spawn our gltf as a scene under it
-    commands.spawn_bundle((
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        GlobalTransform::identity(),
-    )).with_children(|parent| {
+    commands.spawn_bundle(earth_parent).with_children(|parent| {
         parent.spawn_scene(earth_model);
     });
 
     // camera
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-3., -3., -3.).looking_at(earth_parent.0.translation, Vec3::Y),
         ..Default::default()
     });
 }
