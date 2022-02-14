cfc6329601a47deb1daf8f672e10d25e3c672147
Calvin <calvinquach@gmail.com>
2022-02-13 22:09:09 -0500

    update: iterate data recieved and reorientate camera
    reposition camera and orientation so that objects can be added along
    x-axis
    
    todo:
    - spawn objects along x-axis based off distance (might want to think
      about scale for our system)
    - update the ui so that they can only request data for 1 day

--- app/src/main.rs
+++ app/src/main.rs
@@ -24,7 +24,7 @@ fn initialize_world(
 
     // camera
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-3., -3., -3.).looking_at(earth_parent.0.translation, Vec3::Y),
+        transform: Transform::from_xyz(0., 0., -5.).looking_at(earth_parent.0.translation, Vec3::Y),
         ..Default::default()
     });
 }
@@ -88,11 +88,23 @@ fn controls_ui(
 
 fn read_new_near_earth_object_data_stream(
     mut data_receiver: ResMut<NearEarthObjectDataReciever>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     match data_receiver.0.try_recv() {
         Ok(v) => {
             info!("New near earth object data! {:?}", v.element_count);
             // todo: send event to spawn asteroids?
+            for (date, neo_objects) in v.near_earth_objects.iter() {
+                info!("date: {} num_objects: {}", date, neo_objects.len());
+                commands.spawn_bundle(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 0.1, subdivisions: 10})),
+                    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                    transform: Transform::from_xyz(1., 0., 0.),
+                    ..Default::default()
+                });
+            }
         },
         Err(tokio::sync::mpsc::error::TryRecvError::Empty) => (),
         Err(e) => {
@@ -125,7 +137,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
             match request_data_receiver.recv().await {
                 Some(date_range) =>{
                     println!("Got message in tokio: {:?}", date_range);
-                    if let Ok(response) = near_earth_object_client.get_near_earth_objects(&date_range.start_date, &date_range.end_date).await {
+                    // default values of empty
+                    let start_date = if date_range.start_date.is_empty() {
+                        "2020-01-01"
+                    } else {
+                        &date_range.start_date
+                    };
+                    let end_date = if date_range.end_date.is_empty() {
+                        "2020-01-01"
+                    } else {
+                        &date_range.end_date
+                    };
+
+                    if let Ok(response) = near_earth_object_client.get_near_earth_objects(start_date, end_date).await {
                         if let Err(_) = response_data_sender.send(response) {
                             println!("The reciever dropped for response_data");
                         }
@@ -133,7 +157,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                         println!("Error when trying to call api with date_range={:?}", date_range)
                     }
                 },
-                None => println!("sender dropped")
+                None => ()
             }
         }
     });
